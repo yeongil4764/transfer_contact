4fee6b32f6a1539aefae2366121e28a02f4e0a03
giri <ho1325646@gmail.com>
2021-09-10 10:11:01 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  var cors = require("cors");
-  // "https://address-api2.herokuapp.com"
-  const corsOptions = {
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  }
   app.enableCors();
-  app.use(cors(corsOptions));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
