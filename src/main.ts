dfde0f86cab67cd88264f68c2186b7be0098de9a
giri <ho1325646@gmail.com>
2021-09-10 09:34:40 +0900

    cookie 설정

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  var cors = require("cors");
+  const corsOptions = {
+    origin: ["https://address-api2.herokuapp.com"],
+    credentials: true,
+  }
   app.enableCors();
+  app.use(cors(corsOptions));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
