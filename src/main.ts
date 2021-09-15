eae81513989fb32251238f90f2553871730b0187
giri <ho1325646@gmail.com>
2021-09-15 17:24:51 +0900

    qqq

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    credentials: true
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
