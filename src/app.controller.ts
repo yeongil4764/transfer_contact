3e5ee2890c9d7eaa8061ab3fe7781f6cf7e98410
giri <ho1325646@gmail.com>
2021-09-27 10:23:40 +0900

    cookie

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,16 @@
 import { Prisma, User } from '.prisma/client';
-import { Body, Controller, Delete, Param, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  Request,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Response } from 'express';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 
@@ -20,16 +30,28 @@ export class AppController {
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
-  login(@Request() req): any {
-    return this.authService.login(req.user);
+  async login(
+    @Request() req,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<any> {
+    const { accessToken, expireAt, rtid, user } = await this.authService.login(
+      req.user,
+    );
+    const expires = new Date();
+    expires.setDate(Date.now() + 1000 * 60 * 15);
+
+    res.cookie('accessToken', accessToken, {
+      path: '/',
+      httpOnly: false,
+      expires,
+      secure: true,
+    });
+
+    return await accessToken;
   }
 
   @Post('rt')
-  async exToken(@Body() info: {
-    id: number,
-    name: string,
-  }): Promise<any> {
+  async exToken(@Body() info: { id: number; name: string }): Promise<any> {
     return await this.authService.exToken(info);
   }
-
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(cookieParser());
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
